ce0bc3ce6bf0aecd397d13237d5994238ee69cec
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-04-09 16:14:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 exports.config = {
     // bot owner discord id
-    OWNER_ID: "101041126537973760",
+    OWNER_ID: "537147937583529994",
 
     // bot prefix
-    PREFIX: ".",
+    PREFIX: "+",
 
     // channel id's to ignore
-    IGNORE_CHANNELS: ["397961894654246913"],
+    IGNORE_CHANNELS: ["564951666802032640"],
 
     // where to send welcome message (guildID : channelID)
     WELCOME_MESSAGE_CHANNEL: {
-        "408735794094669824": "408737565298917397",
+        "408735794094669824": "564951512888115203",
         //"144518146232025088": "193151740743385089"
     }
 };
